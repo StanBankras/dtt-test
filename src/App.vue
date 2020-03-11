cb6138ca8daeb5963e3be978fb732d3dced9759f
Stan <Stanbankras@gmail.com>
2020-03-11 19:34:33 +0100

    Vue setup ready

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <app-layout>
+      <router-view/>
+    </app-layout>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+import AppLayout from '@/components/layout/AppLayout.vue';
+
+export default Vue.extend({
+  components: {
+    AppLayout
+  }
+});
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/components/layout/AppLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="layout">
+    <page-header></page-header>
+    <slot></slot>
+    <page-footer></page-footer>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+import PageHeader from '@/components/PageHeader.vue';
+import PageFooter from '@/components/PageFooter.vue';
+
+export default Vue.extend({
+  components: {
+    PageHeader,
+    PageFooter
+  }
+})
+</script>
