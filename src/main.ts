5ee66612282e6693a16f6bcd9a192353eb41e230
Stan <Stanbankras@gmail.com>
2020-03-12 18:44:40 +0100

    Installed Vuex store

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import Vue from 'vue';
+import Vuex from 'vuex'
 import App from './App.vue';
 import router from './router';
 import Axios from 'axios';
 
+// Global components
 import Title from '@/components/Title.vue'
-
 Vue.component('title-element', Title)
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    loadedGames: []
+  }
+})
 
 Vue.config.productionTip = false;
 Vue.prototype.$axios = Axios;
@@ -14,5 +23,6 @@ Vue.prototype.$axios = Axios;
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app');
